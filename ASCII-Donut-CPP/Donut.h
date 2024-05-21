53ada69a5df810dc83f68a27068eac697133911f
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-05-20 21:41:23 -0400

    Project Initialization

---
+++ ASCII-Donut-CPP/Donut.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+
+class Donut
+{
+
+
+public:
+
+	// Constructor
+// Initialize the variables
+// Set the values of the variables
+// Allocate memory
+// Open files
+// Connect to a database
+// etc.
+// Donut::Donut() is a constructor
+// Donut::Donut() is a member function
+// Donut::Donut() is a method
+
+
+
+
+
+
+
+
+	Donut()
+	{
+
+	}
+
+
+
+	void drawDonut()
+	{
+		std::cout << "Hello World!\n";
+	}
+
+
+
+
+
+
+};
+
